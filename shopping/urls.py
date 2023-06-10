367727279e5c2f320c7aa551d34b76954d8a5836
立夫 李 <gaexpa148124@gmail.com>
2023-06-11 02:52:32 +0800

    index頁面基本功能(最新商品、熱賣商品)完成 左側linkbar連結有了 新增的資料都是Product相關 model.py裡的Order_Product有增加屬性amount 其他左側分類查詢、詳細頁面、base.html還沒

--- shopping/urls.py
+++ shopping/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('index/', views.index, name='index'),
+    path('index/category/<str:category>', views.category_search, name='category'),
+    path('index/details/<int:id>', views.details, name='details'),
 ]
--- shopping/views.py
+++ shopping/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from .models import Customer, Product, ProductPhoto, ProductPhoto_Product, FollowList, FollowList_Product, ShoppingCart, ShoppingCart_Product, Order, Order_Product, Comment, ShippingInfo, PaymentInfo
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello world!")
+    template = loader.get_template('index.html')
+    new_products = Product.objects.raw('SELECT * FROM shopping_product GROUP BY name ORDER BY id DESC LIMIT 10')
+    #new_products = Product.objects.order_by("-id").values('name').distinct()[:10]
+    best_selling_products = Product.objects.raw('SELECT id, name, category, price, SUM(sold_quantity) AS sold_quantity, photo FROM shopping_product GROUP BY name ORDER BY sold_quantity DESC LIMIT 10')
+    #best_selling_products = Product.objects.order_by("-sold_quantity").values('name').distinct("name")[:10]
+
+    context = {
+        'new_products' : new_products,
+        'best_selling_products' : best_selling_products,
+    }
+    return HttpResponse(template.render(context, request))
+
+def category_search(request):
+    pass
+
+def details(request):
+    pass
