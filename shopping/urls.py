acb529b2aa49117c86afc5b59f27cdb1623ea82d
立夫 李 <gaexpa148124@gmail.com>
2023-06-13 01:30:53 +0800

    全merge 但購物流程到後面結帳還有問題 也還沒有  商品加入購物車的function

--- shopping/urls.py
+++ shopping/urls.py
@@ -11,4 +11,10 @@ urlpatterns = [
     path('index/category/<str:category>', views.category_search, name='category'),
     path('index/details/<int:id>', views.details, name='details'),
     path('search/', views.search, name='search'),
+
+    path("cart/", views.view_cart, name="cart"),   #查看購物車
+    path("delete/<int:item_id>/", views.delete_item, name="delete_item"),    #刪除購物車內商品
+    path("check_order/", views.check_order, name="check_order"),     #在購物車內點結帳後，顯示
+    path("checkout/", views.checkout, name="checkout"),      #
+    path("order/", views.order, name="order"),
 ]
--- shopping/views.py
+++ shopping/views.py
@@ -207,3 +207,92 @@ def search(request):
         'products': products,
     }
     return render(request, 'search.html', context=context)
+
+
+
+def view_cart(request):
+    if request.user.is_authenticated:
+        customer = Customer.objects.get(user=request.user)
+        cart_items = customer.shoppingcart.product.all()
+
+        context = {
+            "cart_items": cart_items,
+        }
+        return render(request, "cart.html", context)
+    else:
+        return redirect("login")  # 重定向到登录页面
+
+
+def delete_item(request, item_id):
+    if request.user.is_authenticated:  # 检查用户是否已登录
+        customer = Customer.objects.get(user=request.user)
+        shopping_cart = customer.shoppingcart
+        item = shopping_cart.product.get(id=item_id)
+        shopping_cart.product.remove(item)
+        return redirect("cart")
+    else:
+        return redirect("login")  # 重定向到登录页面
+
+
+def check_order(request):
+    if request.user.is_authenticated:
+        customer = Customer.objects.get(user=request.user)
+        user = customer.user
+        cart_items = customer.shoppingcart.product.all()
+        order_items = customer.order.product.all()
+        first_order = Order.objects.filter(customer=customer.id).first()
+        amounts = Order_Product.objects.filter(order=first_order.id).all()
+
+        total_price = 0
+        for item in cart_items:
+            total_price += item.price
+
+        context = {
+            "customer": customer,
+            "cart_items": cart_items,
+            "user": user,
+            "amounts": amounts,
+            "order_items": order_items,
+            "total_price": total_price,
+        }
+        return render(request, "checkout.html", context)
+    else:
+        return redirect("cart")
+
+
+def checkout(request):
+    if request.user.is_authenticated:
+        customer = Customer.objects.get(user=request.user)
+        shopping_cart = customer.shoppingcart
+        cart_items = customer.shoppingcart.product.all()
+
+        unfinished_orders = Order.objects.filter(customer=customer, status="unfinished")
+        if unfinished_orders.exists():
+            order = unfinished_orders.first()
+        else:
+            # 创建新的订单
+            order = Order.objects.create(customer=customer)
+
+        if cart_items.exists():
+            # 将购物车商品添加到订单中
+            for item in cart_items:
+                Order_Product.objects.create(order=order, product=item, amount=1)
+            for item in cart_items:
+                shopping_cart.product.remove(item)
+            # 重定向到结账页面
+            return redirect("order")
+        else:
+            # 购物车为空，无法结账
+            return redirect("cart")
+    else:
+        return redirect("index")
+
+
+
+def order(request):
+    customer = Customer.objects.get(user=request.user)
+    order_items = customer.order.product.all()
+    context = {
+        "order_items": order_items,
+    }
+    return render(request, "order.html", context)
